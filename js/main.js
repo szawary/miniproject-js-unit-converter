49543f980aea5fd3947f60fb36daa0e465bfc69a
Szabo Laszlo <szabo.laszlo@szawary.eu>
2020-11-22 13:17:52 +0100

    converter is ready

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
-function calcClick() {
-    let feets = document.querySelector('.feet');
-    let inches = document.querySelector('.inches');
+const btn = document.querySelector('.button');
 
-    let cm = (parseInt(feets.value)) * 2.54 + (parseInt(inches.value) / 100 * 2.54);
-    console.log(cm);
-}
+(function clickButton() {
 
+    btn.addEventListener(
 
+        'click', function calcClick() {
+
+            let feets = document.querySelector('.feet');
+            let inches = document.querySelector('.inches');
+
+            let cm = (parseInt(feets.value)) * 2.54 + (parseInt(inches.value) / 100 * 2.54);
+            let resultAmount = document.querySelector('.resultAmount');
+
+            return resultAmount.innerHTML = cm + 'cm';
+
+        }
+
+    );
+
+})();
