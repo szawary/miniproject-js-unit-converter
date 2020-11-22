d7b88e37d1a4ac1ac61e1841c00c923510fac8e9
Szabo Laszlo <szabo.laszlo@szawary.eu>
2020-11-22 12:36:46 +0100

    calculate funciton is ready

--- js/main.js
+++ js/main.js
@@ -1,8 +1,9 @@
-let feets = document.querySelector('.feet');
-let inches = document.querySelector('.inches');
+function calcClick() {
+    let feets = document.querySelector('.feet');
+    let inches = document.querySelector('.inches');
 
-let cm = parseInt(feets.value) * 2.54 + parseInt(inches.value) / 100 * 2.54;
-
-
+    let cm = (parseInt(feets.value)) * 2.54 + (parseInt(inches.value) / 100 * 2.54);
+    console.log(cm);
+}
 
 
